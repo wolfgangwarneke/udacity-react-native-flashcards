40dba0961c68d3df93f11a8da637f597607f131c
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2017-12-30 04:30:00 -0800

    chore: add colorsheet
    A utils file for storing colors and store some colors in it.

---
+++ utils/colors.js
@@ -0,0 +1,10 @@
+// branding colors
+export const aqua = 'rgba(0, 255, 191, 1)'
+export const skyBlue = 'rgba(124, 254, 240, 1)'
+export const orange = 'rgba(255, 153, 0, 1)'
+export const fadedOrange = 'rgba(252, 203, 133, 1)'
+export const offWhite = 'rgba(234, 247, 207, 1)'
+
+// general colors
+export const white = 'rgba(255, 255, 255, 1)'
+export const black = 'rgba(0, 0, 0, 1)'
