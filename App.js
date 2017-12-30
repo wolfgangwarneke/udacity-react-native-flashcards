e394be375ab66cf5eaf0df80a774cd8ebfda1403
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2017-12-30 00:56:34 -0800

    feat: create navigation framework
    Create basic implemention of app routing/navigtion with 'react-navigation' using a stack for general and detail views with their own tab sub-navigations.  BUG: backward navigation from the detail view will not go back to the general view if other tabs have been selected. eg. Click to detail, then click add card.  Click back to general home will go to detail home.

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import { FontAwesome } from '@expo/vector-icons'
+
 import Settings from './components/Settings'
 import DeckLibrary from './components/DeckLibrary'
 import AddDeck from './components/AddDeck'
@@ -7,16 +10,104 @@ import DeckDetail from './components/DeckDetail'
 import Quiz from './components/Quiz'
 import AddCard from './components/AddCard'
 
+const GeneralTabs = TabNavigator({
+  DeckLibrary: {
+    screen: DeckLibrary,
+    navigationOptions: {
+      tabBarLabel: 'Deck Library',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='list' size={30} color={tintColor} />
+    },
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus' size={30} color={tintColor} />
+    },
+  },
+  Settings: {
+    screen: Settings,
+    navigationOptions: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='cog' size={30} color={tintColor} />
+    },
+  },
+}, {
+  tabBarOptions: {
+    activeTintColor: 'rgba(0, 0, 255, 1)',
+    style: {
+      height: 56,
+      backgroundColor: 'rgba(0, 255, 60, 1)',
+      shadowColor: 'rgba(0, 0, 0, 1)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
+const DetailTabs = TabNavigator({
+  DeckDetail: {
+    screen: DeckDetail,
+    navigationOptions: {
+      tabBarLabel: 'DeckDetail',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='info' size={30} color={tintColor} />
+    },
+  },
+  Quiz: {
+    screen: Quiz,
+    navigationOptions: {
+      tabBarLabel: 'Start Quiz',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='play' size={30} color={tintColor} />
+    },
+  },
+  AddCard: {
+    screen: AddCard,
+    navigationOptions: {
+      tabBarLabel: 'Add Card',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus' size={30} color={tintColor} />
+    },
+  },
+}, {
+  tabBarOptions: {
+    activeTintColor: 'rgba(140, 100, 0, 0.8)',
+    style: {
+      height: 56,
+      backgroundColor: 'rgba(40, 140, 200, 1)',
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
+const MainNavigator = StackNavigator({
+  General: {
+    screen: GeneralTabs
+  },
+  DeckDetail: {
+    screen: DetailTabs,
+    navigationOptions: {
+      headerTintColor: 'rgba(200, 100, 50, 1)',
+      headerStyle: {
+        backgroundColor: 'rgba(100, 200, 50, 1)'
+      }
+    }
+  }
+})
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <DeckLibrary />
-        <AddDeck />
-        <Settings />
-        <DeckDetail />
-        <Quiz />
-        <AddCard />
+        <MainNavigator />
       </View>
     );
   }
@@ -26,7 +117,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- components/DeckLibrary.js
+++ components/DeckLibrary.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class DeckLibrary extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>This will be the decks list view</Text>
+        <TouchableOpacity
+            onPress={() => this.props.navigation.navigate(
+              'DeckDetail'
+            )}
+        ><Text>Go to Deck Detail Nav</Text></TouchableOpacity>
       </View>
     );
   }
