5e5932154f5d99357a5e50199d77b93cbed30ce6
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2017-12-30 06:03:33 -0800

    feat: adds text input to AddDeck view
    Import and implement TextInput updated local state and using the value to create a new deck by that title upon submission.

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { connect } from 'react-redux'
 import { addDeck } from '../actions'
 
@@ -9,11 +9,16 @@ class AddDeck extends React.Component {
   }
   render() {
     const { dispatch } = this.props
-    const newDeck = {"title": "WOW NEW DECK", "questions": []}
+    const newDeck = {"title": this.state.titleText, "questions": []}
 
     return (
       <View style={styles.container}>
         <Text>This will be the form to create a new deck.</Text>
+        <TextInput
+          style={{height: 80, width: 300, backgroundColor: '#ff4400', fontSize: 50}}
+          placeholder="New title"
+          onChangeText={(titleText) => this.setState({titleText})}
+        />
         <TouchableOpacity onPress={() => dispatch(addDeck(newDeck))}>
           <Text>NEW DECK NOW!</Text>
         </TouchableOpacity>
