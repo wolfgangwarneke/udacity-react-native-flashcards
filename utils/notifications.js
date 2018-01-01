7c3eea24653fbe3f4b53942e938831f8c5d8e3d4
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2018-01-01 03:35:55 -0800

    chore: include notification helper functions
    Include helper functions brought in from the Udacifitness example project. (from https://github.com/udacity/reactnd-UdaciFitness-complete/ by Tyler McGinniss)

---
+++ utils/notifications.js
@@ -0,0 +1,54 @@
+// functions taken from UdaciFitness example app
+
+const NOTIFICATION_KEY = 'NOTIFICATION_KEY_GOES_HERE'
+
+export function clearLocalNotification () {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY)
+    .then(Notifications.cancelAllScheduledNotificationsAsync)
+}
+
+function createNotification () {
+  return {
+    title: "Time to take a quiz?",
+    body: "You haven't taken a quiz yet today. Your decks are waiting—don't fall behind!",
+    ios: {
+      sound: true
+    },
+    android: {
+      sound: true,
+      priority: 'high',
+      sticky: false,
+      vibrate: true
+    }
+  }
+}
+
+export function setLocalNotification () {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync()
+
+              let tomorrow = new Date()
+              tomorrow.setDate(tomorrow.getDate() + 1)
+              tomorrow.setHours(20)
+              tomorrow.setMinutes(0)
+
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day'
+                }
+              )
+
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+            }
+          })
+      }
+    })
+}
