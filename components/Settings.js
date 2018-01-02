92ea469834fa2449e408621568d0019497d7fcb0
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2018-01-02 00:31:59 -0800

    feat: style settings view and add button to clear the local notification
    Adds styling for the previously lackluster Settings component view as well as give it a new button to clear out the local notification (without replacing it).

--- components/Settings.js
+++ components/Settings.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import { resetDecks } from '../actions'
 import { deleteDecks } from '../utils/api'
+import { alertOrange, white, lightBlue } from '../utils/colors'
+import fontPicker from '../utils/fontPicker'
 import { clearLocalNotification } from '../utils/notifications'
 
 class Settings extends React.Component {
-  resetApp = () => {
+  resetDecks = () => {
     const { dispatch } = this.props
     deleteDecks()
       .then(dispatch(resetDecks()))
+  }
+  clearNotification() {
     clearLocalNotification()
   }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.header}>Settings</Text>
-        <Text style={{fontSize: 30}} onPress={this.resetApp}>DELETE ALL THE DECKS</Text>
+        <TouchableOpacity style={styles.button} onPress={this.resetDecks}>
+          <Text style={styles.buttonText}>RESET DECKS</Text>
+        </TouchableOpacity>
+        <Text style={styles.warning}>WARNING: this can NOT be undone.</Text>
+        <TouchableOpacity style={[styles.button, {backgroundColor: lightBlue}]} onPress={this.clearNotification}>
+          <Text style={[styles.buttonText, {fontSize: 19}]}>CLEAR NOTIFICATION</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -27,11 +37,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   header: {
-    fontSize: 20,
-    textDecorationLine: 'underline'
+    fontSize: 40,
+    fontFamily: fontPicker(),
+    textDecorationLine: 'underline',
+    padding: 5,
+    marginBottom: 50
+  },
+  warning: {
+    marginTop: 10,
+    marginBottom: 44
+  },
+  button: {
+    borderWidth: 1,
+    backgroundColor: alertOrange,
+    borderColor: 'rgba(0, 0, 0, 0.2)',
+    padding: 10
+  },
+  buttonText: {
+    color: white,
+    fontSize: 30,
+    fontWeight: 'bold'
   }
 });
 
